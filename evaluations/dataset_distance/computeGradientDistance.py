6e9de2fd2762423800a2a578158e7e5e16909232
Moritz Willig <moritz.willig@gmail.com>
2020-02-29 02:21:39 +0100

    implemented gradient distance evaluation

--- evaluations/dataset_distance/computeGradientDistance.py
+++ evaluations/dataset_distance/computeGradientDistance.py
@@ -1,40 +1,61 @@
+from torch.optim import SGD
+import torch
+from torch.utils.data.dataloader import DataLoader
 
 from flowbias.utils.meta_infrastructure import get_available_datasets, load_model_from_meta, \
     dataset_needs_batch_size_one, get_loss
 
 loss_name = "epe"
-dataset_name = "sintelFinalValid" #flyingChairsValid flyingThingsCleanValid sintelFinalValid kitti2015Valid
+#dataset_name = "sintelFinalValid" #flyingChairsValid flyingThingsCleanValid sintelFinalValid kitti2015Valid
+datasets = get_available_datasets(datasets="subsets", force_mode="test")
 
 models = ["A", "I", "H", "W", "pwcWOX1_chairs", "pwcWOX1_things", "pwcWOX1_sintel", "pwcWOX1_kitti"]  # base models
 
 
 def compute_gradient_score(model, transformer, dataset_name):
-    # computes the average sum of gradients ...
-    datasets = get_available_datasets(restrict_to=[dataset_name], force_mode="test")
-    dataset = datasets[dataset_name]
-
-    loss_args = {
-        "batch_size": 1 if dataset_needs_batch_size_one(dataset_name) else 8
-    }
+    # computes the average sum of gradients
+    batch_size = 1
+    loss_args = { "batch_size": batch_size }
     loss = get_loss(loss_name, model, dataset_name, loss_args=loss_args).cuda()
 
-    # optimizer = optim.SGD(model.parameters(), lr=1.0)
-    for i, sample in enumerate(dataset):
-        if i == 100:
-            break
+    gpuargs = {"num_workers": 4, "pin_memory": False}
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=False, **gpuargs)
 
-        # optimizer.zero_grad()
+    optimizer = SGD(model.parameters(), lr=1.0)
+
+    # create grads
+    grads = {}
+    grad_names = []
+    for paramName, paramValue, in models.named_parameters():
+        grads[paramName] = torch.zeros_like(paramValue)
+        grad_names.append(paramName)
+
+    print(">>", grad_names)
+
+    i=0
+    for sample in loader:
+        #optimizer.zero_grad()
         model.grad.data.zero_()
 
         y = loss(model(transformer(sample)))
         y.backward()
 
-        summed += model.grad
+        i+=1
+
+    for paramName, paramValue, in models.named_parameters():
+        grads[paramName] += paramValue.grad.clone() / len(dataset)
+
+    avg_grad = 0
+    for gradName, grad in grads.items():
+        avg_grad += grad.cpu().numpy().average()
+
+    return avg_grad
 
 
 for model_name in models:
-    model, transformer = load_model_from_meta(model_name)
-    model.cuda().eval()
+    for dataset_name, dataset in datasets.items():
+        model, transformer = load_model_from_meta(model_name)
+        model.cuda().eval()
 
     gradient_score = compute_gradient_score(model, transformer, dataset_name)
-    print(f"{gradient_score}/{dataset_name}: {gradient_score}")
+    print(f"{model_name}@{dataset_name}: {gradient_score}")
